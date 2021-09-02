5d0de9708128d704ede66c69ec843d1323781ad1
Neil Juanillo <neilj1213@outlook.com>
2021-09-02 01:53:05 -0500

    added search history buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,26 +11,24 @@ var $fiveDayForcast4 = $('#5df-4');
 var $fiveDayForcast5 = $('#5df-5');
 var $5DayForcastContainer = $('#5-day-forcast').children();
 var $iconSelector = $('.icon');
+var $searchInput = $('#search-input');
+var $searchButton = $('#search-button');
 var testButton = document.getElementById('testButton');
 
 //error ceased when i wrapped the fetch in a function. Look into that 'does fetch need to be wrapped in functino to work?'
-
-
-
-function testApi(){
+function writeToDashboard(testCity){
     var lon, lat;
+    // var testCity = 'Tulsa';
+    
     // var myApiKey = '2dc0f96b8109c931e6f8ead21fdf1052';
-    var testCity = 'Tulsa';
     var testCurrentDayUrl = `https://api.openweathermap.org/data/2.5/weather?q=${testCity}&units=imperial&appid=2dc0f96b8109c931e6f8ead21fdf1052`;
 
+    //Fetching data for current weather
     fetch(testCurrentDayUrl)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
-            console.log('Made it here');
-            console.log(data);
-
             $cityName.text(data.name);
             $cityTemp.text(data.main.temp);
             $cityWind.text(data.wind.speed);
@@ -38,15 +36,16 @@ function testApi(){
             return data;
         })
         .then(function(data){
+
+            //need to get longitude and latitude coord for next fetch
             lon = data.coord.lon;
             lat = data.coord.lat;
-            console.log(lon);
-            console.log(lat);
 
             var myApiKey = '2dc0f96b8109c931e6f8ead21fdf1052';
             var newfetchUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${myApiKey}`; 
-            console.log(newfetchUrl);
 
+            //This new fetch is added in order to get other data that is not provided in the previous fetch
+            //This new fetch container data about the UV index and data we need for the 5-day forecast and icon
             return fetch(newfetchUrl).then(function (response) {
                 return response.json();
             })
@@ -54,7 +53,7 @@ function testApi(){
                 console.log(data.daily);
                 $cityUV.text(data.current.uvi);
 
-                console.log($5DayForcastContainer); 
+                // console.log($5DayForcastContainer); 
 
                 $5DayForcastContainer.each( function( index ) {
                     var dayEl = $5DayForcastContainer[index];
@@ -65,46 +64,36 @@ function testApi(){
                     $iconSelector.attr('src', iconUrl);
                     console.log($iconSelector[index]);
                    
-                    // createIcon.attributes
-
-                    // console.log(dayEl.children)
-                    // dayEl.children[1].appendChild()
                     dayEl.children[2].innerText = data.daily[index].temp.day;
                     dayEl.children[3].innerText = data.daily[index].wind_speed;
                     dayEl.children[4].innerText = data.daily[index].humidity;
-
-                    
-
-                    
-                    // var getDayInfo = data.daily[index];
-
-                    // var getDayName = getDayInfo.
-                    // var getDayTemp = getDayInfo.
-
-                    // var dayName = dayEl.children('h3');
-                    // var dayTemp = dayEl.children('p')[0];
-                    // var dayWind = dayEl.children('p')[1];
-                    // var dayHumidity = dayEl.children('p')[2];
-
-                    // dayName = 
+                
                 })
-
-                // var testForcastUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${testCity}&cnt=5&appid=58a84a20333ae501fe46a570a111a3d7`;
-
-                // return fetch(testForcastUrl).then(function(response){
-                //    return response.json();
-                // })
-                // .then(function(data){
-                //     console.log(data);
-                // })
             })
         })   
 }
 
-function getUvByCoord(lon, lat){
+function addSearchHistory(name){
+    var pastItem = document.createElement('button');
+    var thisName = name;
+    pastItem.innerText = thisName;
 
-    
-    
+    document.getElementById('search-history').appendChild(pastItem);
+    pastItem.addEventListener('click', showWeather);
 }
 
-testButton.addEventListener('click', testApi);
+function showWeather(){ 
+    writeToDashboard(this.innerText);
+}
+
+
+$(function(){
+    $searchButton.on('click', function(){
+        var userSearchInput = $searchInput.val();
+        addSearchHistory(userSearchInput);
+        writeToDashboard(userSearchInput);
+    });
+})
+
+
+// testButton.addEventListener('click', writeToDashboard);
