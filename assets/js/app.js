6e0efc510cfe6d1cb1739783c1f731be2ab37818
Neil Juanillo <neilj1213@outlook.com>
2021-09-03 18:55:19 -0500

    added dark theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,9 +30,24 @@ function writeToDashboard(testCity){
         })
         .then(function(data){
             $cityName.text(data.name);
-            $cityTemp.text("Temp: " + data.main.temp);
-            $cityWind.text("Wind(mph): " + data.wind.speed);
-            $cityHumidity.text("Humidity: " + data.main.humidity);
+
+            
+
+            var tempEl = document.getElementById('todayTemp');
+            var windEl = document.getElementById('todayWind');
+            var humidityEl = document.getElementById('todayHum');
+
+            var temp = data.main.temp + "F";
+            var wind = data.wind.speed + "MPH";
+            var humidity = data.main.humidity + "%";
+
+            tempEl.innerText = temp;
+            windEl.innerText = wind;
+            humidityEl.innerText = humidity;
+
+            $cityTemp.append(tempEl);
+            $cityWind.append(windEl);
+            $cityHumidity.append(humidityEl);
             return data;
         })
         .then(function(data){
@@ -50,29 +65,46 @@ function writeToDashboard(testCity){
                 return response.json();
             })
             .then(function(data){
-                console.log(data.daily);
-                $cityUV.text("UV Index: " + data.current.uvi);
+                var uvEl = document.getElementById('todayUv');
+                uvEl.innerText = data.current.uvi + "%";
+                $cityUV.append(uvEl);
 
                 // console.log($5DayForcastContainer); 
 
                 $5DayForcastContainer.each( function( index ) {
                     var dayEl = $5DayForcastContainer[index];
                     var iconcode = data.daily[index].weather[0].icon;
-                    console.log("the icon code is: " + iconcode);
+
                     var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
-                    // var createSpan = document.createElement('span');
-                    // createSpan.innerText = "Temp: " + data.daily[index].temp.day;
-                    // createSpan.innerText = "Wind: " + data.daily[index].wind_speed;
-                    // createSpan.innerText = "Humidity: " + data.daily[index].humidity;
+                    var tempSpanEl = dayEl.children[2].children[0];
+                    var windSpanEl = dayEl.children[3].children[0];
+                    var humiditySpanEl = dayEl.children[4].children[0];
+                    
+                    
+
+                    // var tempSpanEl = document.createElement('span');
+                    // var windSpanEl = document.createElement('span');
+                    // var humiditySpanEl = document.createElement('span');
+                    
+                    var tempSpan = Math.floor(data.daily[index].temp.day) + "F";
+                    var windSpan = Math.floor(data.daily[index].wind_speed)+ "MPH";
+                    var humiditySpan = Math.floor(data.daily[index].humidity) + "%";
+
+                    tempSpanEl.innerText = tempSpan;
+                    windSpanEl.innerText = windSpan;
+                    humiditySpanEl.innerText = humiditySpan;
 
                     $iconSelector.attr('src', iconUrl);
                     console.log($iconSelector[index]);
                    
-                    dayEl.children[2].innerText = "Temp: " + data.daily[index].temp.day;
-                    dayEl.children[3].innerText = "Wind: " + data.daily[index].wind_speed;
-                    dayEl.children[4].innerText = "Humidity: " + data.daily[index].humidity;
-                
+                    dayEl.children[2].innerText = "Temp: ";
+                    dayEl.children[2].appendChild(tempSpanEl);
+
+                    dayEl.children[3].innerText = "Wind: ";
+                    dayEl.children[3].appendChild(windSpanEl);
+
+                    dayEl.children[4].appendChild(humiditySpanEl);
                 })
             })
         })   
@@ -91,7 +123,6 @@ function showWeather(){
     writeToDashboard(this.innerText);
 }
 
-
 $(function(){
     $searchButton.on('click', function(){
         var userSearchInput = $searchInput.val();
@@ -102,3 +133,96 @@ $(function(){
 
 
 // testButton.addEventListener('click', writeToDashboard);
+
+var $themesCtn = $('.themes');
+var defaultTheme = $themesCtn.children()[0];
+
+var defaultEl = document.getElementById('Default');
+var darkEl = document.getElementById('Dark');
+var customEl = document.getElementById('Custom');
+
+
+defaultEl.addEventListener('change', chooseDefaultTheme);
+darkEl.addEventListener('change', chooseDarkTheme);
+// customEl.addEventListener('click', chooseTheme);
+
+function chooseDefaultTheme(){
+    var customBodyColor = document.getElementsByTagName('body')[0];                 //Background Color
+    var customDashboardTop = document.querySelector('.custom-dashboard-top');       //top dashboard background color
+    var customDashboardBottom = document.getElementById('five-day-forecast-ctn');   //bottom dashboard background color
+    var customBgCtn = document.querySelector('.custom-main-container'); 
+    var fiveDay = document.getElementById('five-day-forecast-ctn');
+
+    var fiveDayTextColor = document.querySelectorAll('.forcast-items');
+    var fiveDayTextColorArray = Array.from(fiveDayTextColor);
+    fiveDayTextColorArray.forEach(function(element){
+        element.style.color = 'black';
+    });
+
+    fiveDay.style.background = 'linear-gradient(to right bottom, rgba(255, 255, 255, .8), rgba(255, 255, 255, .3))';
+
+    //Targets the lists and makes font color Black
+    var lists = document.getElementsByTagName('li');
+    var listsArray = Array.from(lists); 
+    listsArray.forEach(function(element){
+        element.style.color = 'black';
+    })          
+
+    //Back background 
+    customBgCtn.style.background = 'linear-gradient(to right bottom, rgba(255, 255, 255, .8), rgba(255, 255, 255, .3))';
+    customBgCtn.style.color = 'black';
+
+    //Dashboard top
+    customDashboardTop.style.background = 'linear-gradient(to right bottom, rgba(255, 255, 255, .9), rgba(255, 255, 255, .1))';
+    customDashboardTop.style.color = 'black';
+
+    //Change background to dark blue gradient
+    customBodyColor.style.background = 'linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)';
+    console.log(customBodyColor);
+}
+
+
+function chooseDarkTheme(){
+
+    var customBodyColor = document.getElementsByTagName('body')[0];
+    var customDashboardTop = document.querySelector('.custom-dashboard-top');       
+    var customDashboardBottom = document.getElementById('five-day-forecast-ctn');  
+    var customBgCtn = document.querySelector('.custom-main-container');
+    var fiveDay = document.getElementById('five-day-forecast-ctn');
+
+
+    var fiveDayTextColor = document.querySelectorAll('.forcast-items');
+    var fiveDayTextColorArray = Array.from(fiveDayTextColor);
+    fiveDayTextColorArray.forEach(function(element){
+        element.style.color = 'white';
+    });
+
+    fiveDay.style.background = 'linear-gradient(to right bottom, rgba(0, 0, 0, .6), rgba(100, 100, 100, .1))';
+
+    //Targets the lists and makes font color Black
+    var lists = document.getElementsByTagName('li');
+    var listsArray = Array.from(lists); 
+    listsArray.forEach(function(element){
+        element.style.color = 'white';
+    });
+    
+    //Back black background
+    customBgCtn.style.background = 'linear-gradient(to right bottom, rgba(0, 0, 0, .3), rgba(0, 0, 0, .1))';
+    customBgCtn.style.color = 'white'
+
+    //Dashboard top
+    customDashboardTop.style.background = 'linear-gradient(to right bottom, rgba(0, 0, 0, .6), rgba(0, 0, 0, .1))';
+    customDashboardTop.style.color = 'white'
+
+
+    customBodyColor.style.background = 'linear-gradient(315deg, #4c4177 0%, #2a5470 74%)';
+    
+
+    
+    console.log(fiveDayTextColor);
+}
+
+// chooseTheme();
+
+// console.log($themeArrayEl);
+
